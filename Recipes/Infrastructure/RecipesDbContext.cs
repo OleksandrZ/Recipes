870c077d52442546e6d678a197b400039a1a8f3c
OleksandrZ <59576709+OleksandrZ@users.noreply.github.com>
2021-01-06 09:20:16 +0200

    Initial commit

---
+++ Recipes/Infrastructure/RecipesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Recipes.Domain;
+
+namespace Recipes.Infrastructure
+{
+    public class RecipesDbContext : IdentityDbContext<User>
+    {
+        public RecipesDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
