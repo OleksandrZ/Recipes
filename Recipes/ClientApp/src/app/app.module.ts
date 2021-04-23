ac3b7216d0e35426a95631e425e281c1e52944f6
OleksandrZ <59576709+OleksandrZ@users.noreply.github.com>
2021-04-23 11:03:34 +0300

    add ng-bootstrap, change modal window display

--- Recipes/ClientApp/src/app/app.module.ts
+++ Recipes/ClientApp/src/app/app.module.ts
@@ -1,35 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { RecipeComponent } from './recipe/recipe.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from "./app.component";
+import { NavMenuComponent } from "./nav-menu/nav-menu.component";
+import { HomeComponent } from "./home/home.component";
+import { RecipeComponent } from "./recipe/recipe.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { NgbAlertModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    RecipeComponent
+    RecipeComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    NgbAlertModule,
+    BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-    { path: '', component: HomeComponent, pathMatch: 'full' },
-    { path: 'recipe/:id', component: RecipeComponent }
-    // { path: 'fetch-data', component: FetchDataComponent },
-], { relativeLinkResolution: 'legacy' }),
-    BrowserAnimationsModule
+    RouterModule.forRoot(
+      [
+        { path: "", component: HomeComponent, pathMatch: "full" },
+        { path: "recipe/:id", component: RecipeComponent },
+        // { path: 'fetch-data', component: FetchDataComponent },
+      ],
+      { relativeLinkResolution: "legacy" }
+    ),
+    BrowserAnimationsModule,
+    NgbModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- Recipes/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Recipes/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,18 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Inject, ViewChild } from "@angular/core";
+import { NgbModal, NgbModalConfig } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-nav-menu',
-  templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  selector: "app-nav-menu",
+  templateUrl: "./nav-menu.component.html",
+  styleUrls: ["./nav-menu.component.css"],
+  providers: [NgbModalConfig, NgbModal],
 })
 export class NavMenuComponent {
-  isExpanded = false;
+  @ViewChild("loginPosition") loginPosition: ElementRef<HTMLElement>;
+  @ViewChild("visibleLogin") visibleLogin: ElementRef<HTMLElement>;
 
-  collapse() {
-    this.isExpanded = false;
+  @ViewChild("registerPosition") registerPosition: ElementRef<HTMLElement>;
+  @ViewChild("visibleRegister") visibleRegister: ElementRef<HTMLElement>;
+
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    // customize default values of modals used by this component tree
   }
 
-  toggle() {
-    this.isExpanded = !this.isExpanded;
+  openSignIn(content) {
+    this.modalService.dismissAll();
+
+    this.visibleLogin.nativeElement.style.display = "block";
+    let modalRef = this.modalService.open(content, {
+      container: this.loginPosition.nativeElement,
+    });
+
+    modalRef.closed.subscribe((result) => {
+      this.visibleLogin.nativeElement.style.display = "none";
+      console.log(result);
+    });
+
+    modalRef.dismissed.subscribe((reason) => {
+      this.visibleLogin.nativeElement.style.display = "none";
+      console.log(reason);
+    });
+  }
+
+  openSignUp(content) {
+    this.modalService.dismissAll();
+
+    this.visibleRegister.nativeElement.style.display = "block";
+    let modalRef = this.modalService.open(content, {
+      container: this.registerPosition.nativeElement,
+    });
+
+    modalRef.closed.subscribe((result) => {
+      this.visibleRegister.nativeElement.style.display = "none";
+      console.log(result);
+    });
+
+    modalRef.dismissed.subscribe((reason) => {
+      this.visibleRegister.nativeElement.style.display = "none";
+      console.log(reason);
+    });
   }
 }
