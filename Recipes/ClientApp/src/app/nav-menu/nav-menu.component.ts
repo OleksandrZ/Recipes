4441e579e1584127a37c7a6fcdc5a8a84a8cbb2c
Oleksandr <59576709+OleksandrZ@users.noreply.github.com>
2021-05-10 13:35:02 +0300

    Add validation to authorization and authentication form

--- Recipes/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Recipes/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,5 +1,20 @@
-import { Component, ElementRef, Inject, ViewChild } from "@angular/core";
-import { NgbModal, NgbModalConfig } from "@ng-bootstrap/ng-bootstrap";
+import {
+  ApplicationRef,
+  Component,
+  ElementRef,
+  Inject,
+  ViewChild,
+} from "@angular/core";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from "@angular/forms";
+import { NgbModal, NgbModalConfig, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "app-nav-menu",
@@ -8,21 +23,93 @@ import { NgbModal, NgbModalConfig } from "@ng-bootstrap/ng-bootstrap";
   providers: [NgbModalConfig, NgbModal],
 })
 export class NavMenuComponent {
-  constructor(config: NgbModalConfig, private modalService: NgbModal) {
-    config.backdrop = 'static';
-    config.keyboard = true;
+  loginForm: FormGroup;
+  registerForm: FormGroup;
+  fieldTextType: boolean;
+
+  constructor(private modalService: NgbModal, private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  // Create login form and register form with validation
+  private createForm() {
+    this.loginForm = this.fb.group({
+      loginEmail: ["", [Validators.required, Validators.email]],
+      loginRememberMe: [""],
+      loginPassword: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(
+            "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
+          ),
+        ],
+      ],
+    });
+
+    this.registerForm = this.fb.group(
+      {
+        registerUsername: ["", [Validators.minLength(4), Validators.required]],
+        registerEmail: ["", [Validators.required, Validators.email]],
+        registerPassword: [
+          "",
+          [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.pattern(
+              "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
+            ),
+          ],
+        ],
+        registerConfirmPassword: [""],
+        registerAcceptTerms: ["", Validators.required],
+      },
+      { validators: passwordMatchValidator }
+    );
   }
 
   openModal(content) {
-    this.modalService.dismissAll();
+    if (this.modalService.hasOpenModals) {
+      this.modalService.dismissAll();
+    }
+
     let modalRef = this.modalService.open(content);
 
-    modalRef.closed.subscribe((result) => {
-      console.log(result);
-    });
+    modalRef.result
+      .then((result) => {
+        console.log("Closed with " + result);
+        if(result === "Sign in"){
 
-    modalRef.dismissed.subscribe((reason) => {
-      console.log(reason);
-    });
+        }
+        else if(result === "Sign up"){
+
+        }
+      }, (reason) => {
+        console.log("Dismissed " + reason);
+        this.loginForm.get("loginPassword").setValue("");
+        this.registerForm.get("registerPassword").setValue("");
+        this.registerForm.get("registerConfirmPassword").setValue("");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
   }
 }
+
+export const passwordMatchValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  const registerPassword = control.get("registerPassword");
+  const registerConfirmPassword = control.get("registerConfirmPassword");
+
+  return registerPassword &&
+    registerConfirmPassword &&
+    registerPassword.value === registerConfirmPassword.value
+    ? null
+    : { passwordMatch: true };
+};
