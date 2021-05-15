5947e8b27931d07b0120676c7e689188526b7349
Oleksandr <59576709+OleksandrZ@users.noreply.github.com>
2021-05-15 12:10:01 +0300

    Fix field name in Comment

---
+++ Recipes/Migrations/20210515090909_Fix field name in Comment table.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Recipes.Migrations
+{
+    public partial class FixfieldnameinCommenttable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdateAt",
+                table: "Comments",
+                newName: "UpdatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "CreateAt",
+                table: "Comments",
+                newName: "CreatedAt");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdatedAt",
+                table: "Comments",
+                newName: "UpdateAt");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Comments",
+                newName: "CreateAt");
+        }
+    }
+}
