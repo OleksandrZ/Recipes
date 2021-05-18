3f01ec6a55c889d7a39d072f4821c7613da67435
Dmitriy Kalashnikov <kalashnikob99@gmail.com>
2021-05-18 21:37:09 +0300

    recipie page

--- Recipes/ClientApp/src/app/core/core.module.ts
+++ Recipes/ClientApp/src/app/core/core.module.ts
@@ -7,27 +7,27 @@ import { JwtInterceptor } from "./interceptors/jwt.interceptor";
 import { UnauthorizedInterceptor } from "./interceptors/unauthorized.interceptor";
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: appInitializer,
-      multi: true,
-      deps: [AuthService],
-    },
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: UnauthorizedInterceptor,
-      multi: true,
-    },
-  ],
+	declarations: [],
+	imports: [CommonModule],
+	providers: [
+		{
+			provide: APP_INITIALIZER,
+			useFactory: appInitializer,
+			multi: true,
+			deps: [AuthService],
+		},
+		{ provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: UnauthorizedInterceptor,
+			multi: true,
+		},
+	],
 })
 export class CoreModule {
-  constructor(@Optional() @SkipSelf() core: CoreModule) {
-    if (core) {
-      throw new Error("Core Module can only be imported to AppModule.");
-    }
-  }
+	constructor(@Optional() @SkipSelf() core: CoreModule) {
+		if (core) {
+			throw new Error("Core Module can only be imported to AppModule.");
+		}
+	}
 }
--- Recipes/ClientApp/src/app/core/services/auth.service.ts
+++ Recipes/ClientApp/src/app/core/services/auth.service.ts
@@ -8,91 +8,91 @@ import { environment } from "src/environments/environment";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
-  private userSubject: BehaviorSubject<ApplicationUser>;
-  public user: Observable<ApplicationUser>;
-  private readonly apiUrl = `${environment.api_url}user`;
+	private userSubject: BehaviorSubject<ApplicationUser>;
+	public user: Observable<ApplicationUser>;
+	private readonly apiUrl = `${environment.api_url}user`;
 
-  private isAuthenticatedSubject = new ReplaySubject<boolean>(1);
-  public isAuthenticated = this.isAuthenticatedSubject.asObservable();
+	private isAuthenticatedSubject = new ReplaySubject<boolean>(1);
+	public isAuthenticated = this.isAuthenticatedSubject.asObservable();
 
-  constructor(private router: Router, private http: HttpClient) {
-    this.userSubject = new BehaviorSubject<ApplicationUser>(null);
-    this.user = this.userSubject.asObservable();
-  }
+	constructor(private router: Router, private http: HttpClient) {
+		this.userSubject = new BehaviorSubject<ApplicationUser>(null);
+		this.user = this.userSubject.asObservable();
+	}
 
-  public get userValue(): ApplicationUser {
-    return this.userSubject.value;
-  }
+	public get userValue(): ApplicationUser {
+		return this.userSubject.value;
+	}
 
-  login(email: string, password: string, rememberMe: boolean) {
-    return this.http
-      .post<any>(
-        `${this.apiUrl}/login`,
-        { email, password, rememberMe },
-        { withCredentials: true }
-      )
-      .pipe(
-        map((user) => {
-          this.userSubject.next(user);
-          console.log(this.userValue);
+	login(email: string, password: string, rememberMe: boolean) {
+		return this.http
+			.post<any>(
+				`${this.apiUrl}/login`,
+				{ email, password, rememberMe },
+				{ withCredentials: true }
+			)
+			.pipe(
+				map((user) => {
+					this.userSubject.next(user);
+					console.log(this.userValue);
 
-          this.isAuthenticatedSubject.next(true);
+					this.isAuthenticatedSubject.next(true);
 
-          this.startRefreshTokenTimer();
-          return user;
-        })
-      );
-  }
+					this.startRefreshTokenTimer();
+					return user;
+				})
+			);
+	}
 
-  logout() {
-    this.http
-      .post<any>(`${this.apiUrl}/logout`, {}, { withCredentials: true })
-      .subscribe();
-    this.isAuthenticatedSubject.next(false);
+	logout() {
+		this.http
+			.post<any>(`${this.apiUrl}/logout`, {}, { withCredentials: true })
+			.subscribe();
+		this.isAuthenticatedSubject.next(false);
 
-    this.stopRefreshTokenTimer();
-    this.userSubject.next(null);
+		this.stopRefreshTokenTimer();
+		this.userSubject.next(null);
 
-    this.router.navigate(["/"]);
-  }
+		this.router.navigate(["/"]);
+	}
 
-  refreshToken() {
-    return this.http
-      .post<any>(`${this.apiUrl}/refresh-token`, {}, { withCredentials: true })
-      .pipe(
-        map(
-          (user) => {
-            this.userSubject.next(user);
-            this.isAuthenticatedSubject.next(true);
-            this.startRefreshTokenTimer();
-            return user;
-          }
-        ),
-        catchError((error) => {
-          this.isAuthenticatedSubject.next(false);
-          return error;
-        })
-      );
-  }
+	refreshToken() {
+		return this.http
+			.post<any>(`${this.apiUrl}/refresh-token`, {}, { withCredentials: true })
+			.pipe(
+				map(
+					(user) => {
+						this.userSubject.next(user);
+						this.isAuthenticatedSubject.next(true);
+						this.startRefreshTokenTimer();
+						return user;
+					}
+				),
+				catchError((error) => {
+					this.isAuthenticatedSubject.next(false);
+					return error;
+				})
+			);
+	}
 
-  // helper methods
+	// helper methods
 
-  private refreshTokenTimeout;
+	private refreshTokenTimeout;
 
-  private startRefreshTokenTimer() {
-    // parse json object from base64 encoded jwt token
-    const jwtToken = JSON.parse(atob(this.userValue.jwtToken.split(".")[1]));
+	private startRefreshTokenTimer() {
+		// parse json object from base64 encoded jwt token
+		const jwtToken = JSON.parse(atob(this.userValue.jwtToken.split(".")[1]));
 
-    // set a timeout to refresh the token a minute before it expires
-    const expires = new Date(jwtToken.exp * 1000);
-    const timeout = expires.getTime() - Date.now() - 60 * 1000;
-    this.refreshTokenTimeout = setTimeout(
-      () => this.refreshToken().subscribe(),
-      timeout
-    );
-  }
+		// set a timeout to refresh the token a minute before it expires
+		const expires = new Date(jwtToken.exp * 1000);
+		const timeout = expires.getTime() - Date.now() - 60 * 1000;
+		this.refreshTokenTimeout = setTimeout(
+			() => this.refreshToken().subscribe(),
+			timeout
+		);
+	}
 
-  private stopRefreshTokenTimer() {
-    clearTimeout(this.refreshTokenTimeout);
-  }
+	private stopRefreshTokenTimer() {
+		clearTimeout(this.refreshTokenTimeout);
+	}
 }
