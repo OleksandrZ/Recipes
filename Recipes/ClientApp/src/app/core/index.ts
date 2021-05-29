ec5c4689b06028296f1099ccfaf43d11bbe0b2d7
Oleksandr <59576709+OleksandrZ@users.noreply.github.com>
2021-05-29 18:21:16 +0300

    Add comment service

--- Recipes/ClientApp/src/app/core/index.ts
+++ Recipes/ClientApp/src/app/core/index.ts
@@ -2,3 +2,4 @@ export * from './guards/auth.guard';
 export * from './services/auth.service';
 export * from './models/application-user';
 export * from './services/recipe.service';
+export * from './services/comment.service';
