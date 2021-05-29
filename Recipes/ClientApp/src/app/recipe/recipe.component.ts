e102ed0f1495d83a0764a0cc0ce81c4d24639251
Oleksandr <59576709+OleksandrZ@users.noreply.github.com>
2021-05-29 18:21:28 +0300

    Add create comment for recipe

--- Recipes/ClientApp/src/app/recipe/recipe.component.ts
+++ Recipes/ClientApp/src/app/recipe/recipe.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
 import { RecipeService } from "../core";
 import { Recipe } from "../core/modules";
+import {
+  CommandComment,
+  CommentService,
+} from "./../core/services/comment.service";
 
 @Component({
   selector: "app-recipe",
@@ -11,23 +16,46 @@ import { Recipe } from "../core/modules";
 export class RecipeComponent implements OnInit {
   recipe: Recipe;
   public recipeLoaded: boolean;
+  private recipeId: string;
+  createCommentForm: FormGroup;
   constructor(
     private route: ActivatedRoute,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private commentService: CommentService,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit() {
-    const recipeId = this.route.snapshot.paramMap.get("id");
+    this.recipeId = this.route.snapshot.paramMap.get("id");
     this.recipeLoaded = false;
-    this.getRecipe(recipeId);
+    this.getRecipe(this.recipeId);
+
+    this.createCommentForm = this.fb.group({
+      body: ["", [Validators.required, Validators.minLength(2)]],
+    });
   }
 
   getRecipe(recipeId: string) {
     this.recipeService.getRecipeById(recipeId).subscribe((recipe) => {
       this.recipe = recipe;
       console.log(this.recipe);
-      
+
       this.recipeLoaded = true;
     });
+  }
+
+  createComment() {
+    if (this.createCommentForm.invalid) {
+      return;
+    }
+
+    let body = this.createCommentForm.value.body;
+    console.log(body);
+    
+    this.commentService
+      .createComment(new CommandComment(this.recipeId, body))
+      .subscribe((comment) => {
+        this.recipe.comments.push(comment);
+      });
   }
 }
